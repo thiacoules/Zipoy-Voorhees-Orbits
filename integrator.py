c29c56ba624bc2f7f2a8e8be96bbdb37893b599f
thiacoules <142110485+thiacoules@users.noreply.github.com>
2026-04-08 19:13:09 +0300

    Create integrator.py

---
+++ integrator.py
@@ -0,0 +1,34 @@
+import autograd.numpy as np
+from autograd import grad
+from scipy.integrate import solve_ivp
+from core_physics import ZVEngine
+
+def get_geodesics(M, gamma, initial_conditions, total_time):
+    engine = ZVEngine(M, gamma)
+    
+    # We define the Hamiltonian gradient (The 'Forces')
+    # This is where autograd shines: it derives the physics automatically!
+    def hamiltonian_func(state_coords, momentum):
+        return engine.hamiltonian(state_coords, momentum)
+
+    # Gradient of Hamiltonian with respect to coordinates
+    dh_dq = grad(engine.hamiltonian, 0)
+    # Gradient of Hamiltonian with respect to momenta
+    dh_dp = grad(engine.hamiltonian, 1)
+
+    def system_dynamics(t, y):
+        # y contains [x, y, px, py]
+        q_vec = y[:2]
+        p_vec = np.array([-0.95, y[2], y[3], 3.0]) # pt, px, py, p_phi
+        
+        # Hamilton's Equations: 
+        # dq/dt = dH/dp
+        # dp/dt = -dH/dq
+        dq_dt = dh_dp(q_vec, p_vec)[1:3] # we only evolve x and y
+        dp_dt = -dh_dq(q_vec, p_vec)
+        
+        return [*dq_dt, *dp_dt]
+
+    # Solving the differential equations
+    sol = solve_ivp(system_dynamics, [0, total_time], initial_conditions, t_eval=np.linspace(0, total_time, 1000))
+    return sol
